4eb87444a0610ff156fdc2640ddae66776c25c43
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-19 15:39:35 +0530

    Add files via upload

---
+++ Lecture11Part1.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int a;
+    cout<<"Size of integer is "<<sizeof(int);
+    cout<<"Size of integer is "<<sizeof(a);
+    return 0;
+}
